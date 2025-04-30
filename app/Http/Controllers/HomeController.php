d51ffa1cc59f9cfaa27bb256f411a4ced9b58c77
Pramudya <pramudyap333@gmail.com>
2025-04-30 22:38:32 +0700

    feat : update home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,16 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
+use App\Models\Contact;
 use App\Models\Service;
 use App\Models\Study;
+use App\Models\Team;
+use App\Models\Testimoni;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $teamposts = Team::get(); 
+        $clientposts = Client::get();
+        $testimoniposts = Testimoni::orderBy('created_at', 'desc')->limit(3)->get();
         $serviceposts = Service::with('author')->get();
         $studyposts = Study::with('author')->get();
-        return view('index', compact('studyposts', 'serviceposts'));
+        $contactposts = Contact::get();
+        return view('index', compact('studyposts', 'serviceposts', 'testimoniposts', 'clientposts', 'teamposts', 'contactposts'));
     }
 }
